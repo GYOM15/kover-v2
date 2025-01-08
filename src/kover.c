c52a6b5b873f7bc5960c3237a892001a008ed01d
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-01-08 08:39:33 -0500

    Réusine kover.c autour sous-commandes

--- src/kover.c
+++ src/kover.c
@@ -3,31 +3,52 @@
 
 #define MAX_LENGTH 50
 
-int main(int argc, char* argv[]) {
+/**
+ * Runs the summarize subcommand
+ */
+void run_summarize_subcommand() {
   char line[MAX_LENGTH + 1];
+  int num_lines = 0;
+  while (fgets(line, MAX_LENGTH, stdin) != NULL)
+    ++num_lines;
+  if (num_lines == 2)
+    puts("An empty scene");
+  else
+    puts("A scene with 1 building");
+}
+
+/**
+ * Runs the describe subcommand
+ */
+void run_describe_subcommand() {
+  char line[MAX_LENGTH + 1];
+  int num_lines = 0;
+  while (fgets(line, MAX_LENGTH, stdin) != NULL)
+    ++num_lines;
+  if (num_lines == 2)
+    puts("An empty scene");
+  else {
+    puts("A scene with 1 building");
+    puts("  building b1 at 0 0 with dimensions 1 1");
+  }
+}
+
+/**
+ * Main function
+ *
+ * @param argc  The number of arguments
+ * @param argv  The arguments
+ */
+int main(int argc, char* argv[]) {
   if (argc < 2)
     return 1;
   const char* subcommand = argv[1];
 
   if (strcmp(subcommand, "summarize") == 0) {
-    int num_lines = 0;
-    while (fgets(line, MAX_LENGTH, stdin) != NULL)
-      ++num_lines;
-    if (num_lines == 2)
-      puts("An empty scene");
-    else
-      puts("A scene with 1 building");
+    run_summarize_subcommand();
     return 0;
   } else if (strcmp(subcommand, "describe") == 0) {
-    int num_lines = 0;
-    while (fgets(line, MAX_LENGTH, stdin) != NULL)
-      ++num_lines;
-    if (num_lines == 2)
-      puts("An empty scene");
-    else {
-      puts("A scene with 1 building");
-      puts("  building b1 at 0 0 with dimensions 1 1");
-    }
+    run_describe_subcommand();
     return 0;
   } else {
     return 1;
