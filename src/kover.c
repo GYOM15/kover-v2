eccda30452bc12927c3ab68c1ac20dbc10d19067
Alexandre Blondin Massé <blondin_masse.alexandre@uqam.ca>
2025-01-08 17:08:20 -0500

    Introduit struct Scene

--- src/kover.c
+++ src/kover.c
@@ -3,35 +3,61 @@
 
 #define MAX_LENGTH 50
 
+// Scenes
+// ------
+
+// A scene
+struct Scene {
+  // The number of building in the scene
+  unsigned int num_buildings;
+};
+
 /**
- * Runs the summarize subcommand
+ * Loads a scene from the standard input
+ *
+ * @param scene  The resulting scene
  */
-void run_summarize_subcommand() {
+void load_scene_from_stdin(struct Scene* scene) {
   char line[MAX_LENGTH + 1];
   int num_lines = 0;
   while (fgets(line, MAX_LENGTH, stdin) != NULL)
     ++num_lines;
-  if (num_lines == 2)
+  scene->num_buildings = num_lines == 2 ? 0 : 1;
+}
+
+// Subcommands processing
+// ----------------------
+
+/**
+ * Runs the summarize subcommand
+ */
+void run_summarize_subcommand() {
+  struct Scene scene;
+  load_scene_from_stdin(&scene);
+  if (scene.num_buildings == 0)
     puts("An empty scene");
   else
-    puts("A scene with 1 building");
+    printf("A scene with %d building%s\n", scene.num_buildings,
+           scene.num_buildings > 1 ? "s" : "");
 }
 
 /**
  * Runs the describe subcommand
  */
 void run_describe_subcommand() {
-  char line[MAX_LENGTH + 1];
-  int num_lines = 0;
-  while (fgets(line, MAX_LENGTH, stdin) != NULL)
-    ++num_lines;
-  if (num_lines == 2)
+  struct Scene scene;
+  load_scene_from_stdin(&scene);
+  if (scene.num_buildings == 0)
     puts("An empty scene");
   else {
-    puts("A scene with 1 building");
+    printf("A scene with %d building%s\n", scene.num_buildings,
+           scene.num_buildings > 1 ? "s" : "");
     puts("  building b1 at 0 0 with dimensions 1 1");
   }
 }
+
+// Main function
+// -------------
 
 /**
  * Main function
